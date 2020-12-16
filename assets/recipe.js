2a1aeea0fafcdbb3872cea7045c9d6596a763b7c
Bas Heerschop <heerschop@gmail.com>
2020-12-16 10:13:22 +0100

    preparation

--- assets/recipe.js
+++ assets/recipe.js
@@ -1,4 +1,21 @@
-function processRecipe() {
+function processRecipes() {
+  for (const element of document.getElementsByTagName('div')) {
+    if (element.hasAttribute && element.hasAttribute("recipe")){
+      const link = document.createElement('a');
+      const text = document.createTextNode(element.firstChild.textContent);
+
+      link.href = element.getAttribute("recipe");
+      link.target = '_blank';
+      link.appendChild(text);
+
+      element.classList.add('recipe');
+      element.removeChild(element.firstChild);
+      element.parentElement.insertBefore(link,element);
+    }
+  }
+}
+
+function processIngredients() {
   for (const ingredients of document.getElementsByClassName('ingredients')) {
     const header = document.createElement("h2");
     const text = document.createTextNode("Ingredients");
@@ -12,7 +29,7 @@ function processRecipe() {
     for (const element of ingredients.childNodes) {
       if (element.getAttribute && element.getAttribute("image")) {
         elements.push(element);
-      }
+      } 
     }
 
     for (const element of elements) {
@@ -39,4 +56,41 @@ function processRecipe() {
   }
 }
 
-processRecipe();
+function processPreparation() {
+  for (const preparation of document.getElementsByClassName('preparation')) {
+    const header = document.createElement("h2");
+    const text = document.createTextNode("Preparation");
+
+    header.appendChild(text);
+
+    preparation.insertBefore(header, preparation.firstChild);
+
+    const elements = [];
+    
+    for (const element of preparation.childNodes) {
+      if (element.getAttribute && element.hasAttribute("bullet")) {
+      console.log(element.getAttribute("bullet"));
+        elements.push(element);
+      }
+    }
+
+    let number = 1;
+    
+    for (const element of elements) {
+      preparation.insertBefore(document.createElement("hr"), element);
+
+      const divElement = document.createElement("div");
+      const text = document.createTextNode(number);
+
+      divElement.appendChild(text);
+
+      element.insertBefore(divElement,element.firstChild);
+      
+      number++;
+    }
+
+    if (elements.length > 0) {
+      preparation.appendChild(document.createElement("hr"));
+    }
+  }
+}
